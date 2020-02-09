1b115066f7bacd88576275c0e56c6e95a778fceb
Simon Kurek <simonkurek@protonmail.com>
2020-02-09 20:38:59 +0100

    Trying sm new

--- src/main/java/com/suhatig/tictactoe/Game.java
+++ src/main/java/com/suhatig/tictactoe/Game.java
@@ -10,6 +10,7 @@ public class Game {
     private int id;
     private State lastState = State.NON;
     private int players = 0;
+    /*private String[][] authKeys;*/
     private HashMap<String, State> authKeys; /*cookie auth code*/
 
     public Game(int id){
@@ -17,7 +18,8 @@ public class Game {
         table = new Table();
         table.clear();
         gameState = GameState.BEFORE;
-        authKeys = new HashMap();
+        authKeys = new HashMap<>();
+        /*authKeys = new String[2][2];*/
     }
 
     public State[][] getAll(String code){
@@ -82,8 +84,12 @@ public class Game {
             String code = Integer.toHexString(r.nextInt(1000000000));
             if (players==0){
                 authKeys.put(code, State.X);
+                /*authKeys[0][0] = code;
+                authKeys[0][1] = String.valueOf(State.X);*/
             } else {
                 authKeys.put(code, State.O);
+                /*authKeys[1][0] = code;
+                authKeys[1][1] = String.valueOf(State.O);*/
             }
             return code;
         }
@@ -92,12 +98,20 @@ public class Game {
 
     public boolean accessBoth(String authCode){
         return authKeys.containsKey(authCode);
+        /*if (authKeys[0][0] == authCode || authKeys[1][0] == authCode){
+            return true;
+        }
+        return false;*/
     }
 
     public State getStateFromCode(String authCode){
-        /*tu coś nie działa + put*/
         System.out.println(authKeys.containsKey(authCode));
+        System.out.println(authKeys.containsValue(authCode));
+        System.out.println(authKeys.get(authCode));
         return authKeys.get(authCode);
+        /*if (authKeys[0][0] == authCode){
+            return (State)authKeys[0][1];
+        }*/
     }
 }
 
